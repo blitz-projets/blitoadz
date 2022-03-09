0c534b17fb25dae848ecfd8599a0539a78e8a4c1
Georges Biaux <georges.biaux@gadz.org>
2022-03-09 11:03:22 +0100

    Add title

--- packages/blitoadz-webapp/src/components/Home/Home.tsx
+++ packages/blitoadz-webapp/src/components/Home/Home.tsx
@@ -2,7 +2,32 @@ import React from "react";
 import Box from "@mui/material/Box";
 
 function Home() {
-  return <Box>home</Box>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "48px",
+      }}
+    >
+      <Box
+        sx={{
+          padding: "48px",
+        }}
+      >
+        <Box
+          sx={{
+            fontFamily: "Snap ITC",
+            marginBottom: "24px",
+            fontSize: "96px",
+          }}
+        >
+          Blitoadz
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
