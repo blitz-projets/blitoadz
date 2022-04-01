86e1c352d8156f9db4172ed0b0f01c2a1e91a52c
Clement Walter <clement.walter@ibm.com>
2022-04-01 14:26:39 +0200

    Update Renderer to replace image_data with image

--- packages/blitoadz-contracts/utils/constants.ts
+++ packages/blitoadz-contracts/utils/constants.ts
@@ -50,6 +50,7 @@ export const loadToadz = (): Toadz[] => {
 // Deploy constants
 export const TAGS = {
   BLITOADZ: "Blitoadz",
+  BLITOADZ_RENDERER_V2: "BlitoadzRendererV2",
   BLITOADZ_PALETTES: "BlitoadzPalettes",
   BLITMAP: "Blitmap",
 };
